3f22d236eca3a8e57f9de2793cfc9d3006b48a63
RamonFern <ramonfernandes9@gmail.com>
2021-03-12 16:06:47 -0300

    Component alunos Criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AlunosComponent } from './alunos/alunos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
